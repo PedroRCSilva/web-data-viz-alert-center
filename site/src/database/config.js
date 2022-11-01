fadfd4e75ea98aced80207bced611e8db6163e89
PedroRCSilva <pedro.rsilva@sptech.school>
2022-11-01 16:10:37 -0300

    subindoAPI

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -3,10 +3,10 @@ var sql = require('mssql');
 
 // CONEXÃO DO SQL SERVER - AZURE (NUVEM)
 var sqlServerConfig = {
-    server: "SEU_SERVIDOR",
-    database: "SEU_BANCO_DE_DADOS",
-    user: "SEU_USUARIO",
-    password: "SUA_SENHA",
+    server: "localhost",
+    database: "",
+    user: "",
+    password: "",
     pool: {
         max: 10,
         min: 0,
@@ -20,9 +20,9 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
-    database: "alertcenter",
-    user: "root",
-    password: "3db01194",
+    database: "",
+    user: "",
+    password: "",
 };
 
 function executar(instrucao) {
